d5a46232572465551ce071585e3d3fd6a7b32ea5
Md. Minar Munshi <minarmunshi46@gmail.com>
2023-11-03 14:50:55 +0600

    Initial Commit

---
+++ prisma/migrations/20231103082657_create_users_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `profile_image_url` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `salt` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
